f2fb4f7ad56c3a6874fb761e8413ac29e85ec74d
Your Name <you@example.com>
2023-09-08 21:48:18 +0530

    few changes StudentDAO

--- src/main/java/dao/custom/StudentDAO.java
+++ src/main/java/dao/custom/StudentDAO.java
@@ -1,8 +1,10 @@
 package dao.custom;
 
 import dao.CrudDAO;
-import dao.SuperDAO;
 import entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO extends CrudDAO<Student> {
+    List<Student> getUnpaidStudents();
 }
