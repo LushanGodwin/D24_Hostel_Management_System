d2653d35ac7c2ee92aa4661fe622b1aba8104b0c
Your Name <you@example.com>
2023-09-08 21:44:40 +0530

    add ReservationTM

---
+++ src/main/java/tm/ReservationTM.java
@@ -0,0 +1,20 @@
+package tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ReservationTM {
+    private String res_id;
+    private String student_id;
+    private String date;
+    private String room_type;
+    private String status;
+
+}
